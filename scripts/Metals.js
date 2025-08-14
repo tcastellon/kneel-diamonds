bea94c3eb66b61bf5371af1ed2992d8da903a861
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-14 16:09:27 -0500

    Updated Metals module

--- scripts/Metals.js
+++ scripts/Metals.js
@@ -1,3 +1,19 @@
 export const MetalOptions = async () => {
-    const response = await fetch()
+    const response = await fetch("http://localhost:8088/metals")
+    const metals = await response.json()
+
+    let html = `
+        <div class="metal-input">
+            <h2>Metals</h2>
+    `
+
+    //Generate radio buttons for each metal option and add to html
+    for (const metal of metals) {
+        html += `
+            <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}`
+    }
+
+    html += `
+        </div>
+    `
 }
