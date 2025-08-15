107923e47ef70e63577f75e8450612f2ffbdf94e
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-15 11:54:05 -0500

    Updated SizeOptions module

--- scripts/SizeOptions.js
+++ scripts/SizeOptions.js
@@ -2,19 +2,25 @@ export const SizeOptions = async () => {
     const response = await fetch("http://localhost:8088/sizes")
     const sizes = await response.json()
 
-    let optionsHTML = `
-        <div class="size-input">
-    `
+    let optionsHTML = ``
 
-    //Generate radio buttons for each size option and add to optionsHTML
-    for (const size of sizes) {
-        optionsHTML += `
-            <input type="radio" name="size" value="${size.id}"/> ${size.carets}ct
-        `
-    }
+    //Using a for..of loop to generate radio buttons for each size option and add to optionsHTML
+    // for (const size of sizes) {
+    //     optionsHTML += `
+    //         <input type="radio" name="size" value="${size.id}"/> ${size.carets}ct
+    //     `
+    // }
 
-    optionsHTML += `
-        </div>
-    `
+    //Using map() to generate a new array of strings
+    const divStringArray = sizes.map(
+        (size) => {
+            return `<div>
+                <input type="radio" name="size" value="${size.id}"/> ${size.carets}ct
+            </div>`
+        }
+    )
+    //Joins the array of strings into a single string
+    optionsHTML += divStringArray.join("")
+
     return optionsHTML
 }
