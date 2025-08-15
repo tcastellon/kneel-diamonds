11416c304d75253680f89ecb65d79b5bd8856b7f
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-15 10:04:24 -0500

    Updated Main module

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+import { MetalOptions } from "./Metals.js"
+
+const container = document.querySelector("#container")
+
+const render = async () => {
+    const metalOptionsHTML = await MetalOptions()
+
+    const composedHTML = `
+        <h1></h1>
+
+        <article class="choices">
+            <section class="choices_metals options">
+                <h2>Metals</h2>
+                ${metalOptionsHTML}
+            </section>
+
+            <section class="choices_sizes options">
+                <h2>Sizes</h2>
+
+            </section>
+            <section class="choices_styles options">
+                <h2>Styles</h2>
+
+            </section>
+        </article>
+        
+        <article class="order">
+
+        </article>
+        
+        <article class="customOrders">
+            <h2>Custom Jewelry Orders</h2>
+
+        </article>
+    `
+    container.innerHTML = composedHTML
+}
+
+render()
