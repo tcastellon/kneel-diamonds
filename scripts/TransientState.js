437841aac1f48ed463715ae19cf354766ad9841f
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-15 13:43:55 -0500

    Updated TransientState module

--- scripts/TransientState.js
+++ scripts/TransientState.js
@@ -3,3 +3,15 @@ const transientState = {
     carats: 0,
     metal: ""
 }
+
+export const setStyle = (chosenStyle) => {
+    transientState.style = chosenStyle
+}
+
+export const setCarats = (chosenCarats) => {
+    transientState.carats = chosenCarats
+}
+
+export const setMetal = (chosenMetal) => {
+    transientState.metal = chosenMetal
+}
