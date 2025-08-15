7daf6851566db216ad0e48c3415fc8446df9a809
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-15 11:53:43 -0500

    Updated MetalOptions module

--- scripts/MetalOptions.js
+++ scripts/MetalOptions.js
@@ -2,19 +2,25 @@ export const MetalOptions = async () => {
     const response = await fetch("http://localhost:8088/metals")
     const metals = await response.json()
 
-    let optionsHTML = `
-        <div class="metal-input">
-    `
+    let optionsHTML = ``
 
-    //Generate radio buttons for each metal option and add to optionsHTML
-    for (const metal of metals) {
-        optionsHTML += `
-            <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
-        `
-    }
+    //Using a for..of loop to generate radio buttons for each metal option and add to optionsHTML
+    // for (const metal of metals) {
+    //     optionsHTML += `
+    //         <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
+    //     `
+    // }
 
-    optionsHTML += `
-        </div>
-    `
+    //Using map() to generate a new array of strings
+    const divStringArray = metals.map(
+        (metal) => {
+            return `<div>
+                <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
+            </div>`
+        }
+    )
+    //Joins the array of strings into a single string
+    optionsHTML += divStringArray.join("")
+
     return optionsHTML
 }
